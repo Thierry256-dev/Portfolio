29b2c7ccd4814716654d78d01455870b12d726ff
Thierry256-dev <thierrymunderi@gmail.com>
2025-06-23 12:41:40 +0300

    adding testimonial component and importing components into the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./Componets/Home";
+import About from "./Componets/About";
+import Projects from "./Componets/Projects";
+import Education from "./Componets/Education";
+import Testimonials from "./Componets/Testimonials";
+import Footer from "./Componets/Footer";
 
 function App() {
   return <></>;
