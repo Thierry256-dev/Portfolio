e68c706d003d7ec56c72603241835c96f03d68a6
Thierry256-dev <thierrymunderi@gmail.com>
2025-06-23 13:52:33 +0300

    adding content to home and about

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -1,3 +1,27 @@
 export default function About() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          I'm Thierry Munderi, a frontend developer with a strong passion for
+          crafting responsive, accessible, and high-performance web
+          applications. I specialize in JavaScript and modern frameworks like
+          React, with a keen eye for clean UI design and seamless user
+          experiences.
+        </p>
+        <p>
+          My workflow combines technical precision with creative
+          problem-solving--whether it's building reusable components, optimizing
+          performance, or integraing APIs. I thrive in fast-paced environments
+          where quality, scalability, and maintainability are priorites.
+        </p>
+        <p>
+          I love turning ideas into functional, beautiful interfaces-- and I'm
+          always looking for new challenges that push the boundaries that the
+          web can do.
+        </p>
+      </div>
+    </>
+  );
 }
--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="flex justify-around items-center p-10px">
         <h1>Thierry_Munderi</h1>
-        <nav>
+        <nav className="flex gap-4">
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
@@ -12,7 +12,19 @@ export default function Home() {
           <a href="#footer">Contacts</a>
         </nav>
       </div>
-      <div></div>
+      <div>
+        <div>
+          <h1>Hi, I'm Thierry Munderi</h1>
+          <h2>Frontend Developer | UI Engineer | Problem Solver</h2>
+          <p>
+            I build responsive, performant, and user-focused web applications
+            using modern Frontend technologies like JavaScript, React and
+            Tailwind CSS. Bringing clean design and efficient code together to
+            create intuitive digital experiences
+          </p>
+          <button>Contact Me</button>
+        </div>
+      </div>
     </>
   );
 }
